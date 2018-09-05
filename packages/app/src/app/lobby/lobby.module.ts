efe561ee82edde9432a0c56f4f86eca4fd8a4917
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-05 17:58:00 +0300

    Add Bootstrap, theming and lobby layout

--- packages/app/src/app/lobby/lobby.module.ts
+++ packages/app/src/app/lobby/lobby.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { LobbyComponent } from "./pages/lobby/lobby.component";
 import { LobbyRouting } from "./lobby.routing";
+import {
+  MatCardModule,
+  MatIconModule,
+  MatButtonModule,
+  MatToolbarModule
+} from "@angular/material";
 
 @NgModule({
-  imports: [CommonModule, LobbyRouting],
+  imports: [
+    CommonModule,
+    LobbyRouting,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule
+  ],
   declarations: [LobbyComponent]
 })
 export class LobbyModule {}
