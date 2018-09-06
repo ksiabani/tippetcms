770fbc1b3b96694f77c3878810c4c277a8df2517
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-06 17:32:20 +0300

    WIP: Handle dialog data

--- packages/app/src/app/lobby/pages/lobby/lobby.component.ts
+++ packages/app/src/app/lobby/pages/lobby/lobby.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Select } from "@ngxs/store";
 import { Store } from "@ngxs/store";
 import { LoginState } from "../../../login/store/login.state";
@@ -8,28 +8,33 @@ import { MatDialog } from "@angular/material";
 import { AddProjectDialogComponent } from "../add-project-dialog/add-project-dialog.component";
 
 @Component({
-  selector: 'app-lobby',
-  templateUrl: './lobby.component.html',
-  styleUrls: ['./lobby.component.scss']
+  selector: "app-lobby",
+  templateUrl: "./lobby.component.html",
+  styleUrls: ["./lobby.component.scss"]
 })
 export class LobbyComponent implements OnInit {
-
   @Select(LoginState.user)
   user: Observable<firebase.User>;
+  siteName: string;
+  siteTemplate: string;
 
   constructor(private store: Store, public dialog: MatDialog) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   logout() {
     this.store.dispatch(Logout);
   }
 
   openDialog(): void {
-    this.dialog.open(AddProjectDialogComponent, {
+    const dialogRef = this.dialog.open(AddProjectDialogComponent, {
       disableClose: true,
-      panelClass: 'add-project-dialog'
+      panelClass: "add-project-dialog",
+      data: {siteName: this.siteName, siteTemplate: this.siteTemplate}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(result);
     });
   }
 }
