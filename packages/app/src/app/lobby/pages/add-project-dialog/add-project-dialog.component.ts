d8f3ebcc20434405b7afb807aa9784722a31ca34
kostas.siabanis@gmail.com <kostas.siabanis@gmail.com>
2018-09-06 17:32:20 +0300

    WIP: Handle dialog data

--- packages/app/src/app/lobby/pages/add-project-dialog/add-project-dialog.component.ts
+++ packages/app/src/app/lobby/pages/add-project-dialog/add-project-dialog.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  Inject} from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
 
+export interface newProjectData {
+  siteName: string;
+  siteTemplate: string;
+}
+
 @Component({
-  selector: 'app-add-project-dialog',
-  templateUrl: './add-project-dialog.component.html',
-  styleUrls: ['./add-project-dialog.component.scss']
+  selector: "app-add-project-dialog",
+  templateUrl: "./add-project-dialog.component.html",
+  styleUrls: ["./add-project-dialog.component.scss"]
 })
 export class AddProjectDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<AddProjectDialogComponent>) {}
+  constructor(
+    public dialogRef: MatDialogRef<AddProjectDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: newProjectData
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   close(): void {
     this.dialogRef.close();
   }
-
 }
