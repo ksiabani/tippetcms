0ec557571e44c59020442e2fc7d2be6203d97c21
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-07 14:02:06 +0300

    get sites

---
+++ packages/api/src/api/lobby/lobby.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, HttpCode, Param } from '@nestjs/common';
+import { readdirSync, existsSync } from 'fs';
+import { join } from 'path';
+import { mkdirpSync } from 'fs-extra';
+
+@Controller('lobby')
+export class LobbyController {
+  constructor() {}
+
+  @Get(':username')
+  getSites(
+    @Param('username') username,
+  ): { existed: boolean; created: boolean; sites?: string[] } {
+    const publicDirToCheck = join(__dirname, '../..', 'public', username);
+    const gutsbiesDirToCheck = join(__dirname, '../..', 'gutsbies', username);
+
+    if (existsSync(publicDirToCheck) && existsSync(gutsbiesDirToCheck)) {
+      const sites = readdirSync(join(__dirname, '../..', 'gutsbies')).filter(
+        dir => dir !== '.keep',
+      );
+      return { existed: true, created: false, sites };
+    }
+    mkdirpSync(publicDirToCheck);
+    mkdirpSync(gutsbiesDirToCheck);
+    return { existed: false, created: true };
+  }
+}
