e103b128322f589f1a670f7f95e70b6f8b274ee0
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-10 14:59:40 +0300

    add sites

--- packages/app/src/app/lobby/services/lobby.service.ts
+++ packages/app/src/app/lobby/services/lobby.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
 import { Observable } from "rxjs";
 import { GetSitesResponse } from "../../shared/model/get-sites.interface";
+import { NewProjectData } from "../components/add-project-dialog/add-project-dialog.component";
 
 @Injectable({
   providedIn: "root"
@@ -12,5 +13,9 @@ export class LobbyService {
 
   getSites(username): Observable<GetSitesResponse> {
     return this.http.get<GetSitesResponse>(`${environment.api.lobby}/${username}`);
+  }
+
+  addSite(newProjectData: NewProjectData): Observable<{ sucess: boolean }> {
+    return this.http.post<{ sucess: boolean }>(environment.api.lobby, newProjectData);
   }
 }
