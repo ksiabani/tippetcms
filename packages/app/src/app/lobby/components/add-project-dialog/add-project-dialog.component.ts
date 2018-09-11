b4130d8288559152972954327eaa7da7f08d5264
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-11 13:28:43 +0300

    sites name validator

--- packages/app/src/app/lobby/components/add-project-dialog/add-project-dialog.component.ts
+++ packages/app/src/app/lobby/components/add-project-dialog/add-project-dialog.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Inject } from "@angular/core";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
-import { Select, Store } from "@ngxs/store";
-import { LoginState } from "../../../login/store/login.state";
-import { User } from "../../../shared/model/user.interface";
-import { Observable } from "rxjs/internal/Observable";
+import { Store, Select } from "@ngxs/store";
 import { AddSite } from "../../store/lobby.actions";
+import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import { LobbyState } from "../../store/lobby.state";
+import { Observable } from "rxjs";
 
 export interface NewProjectData {
   siteName: string;
@@ -18,13 +18,38 @@ export interface NewProjectData {
   styleUrls: ["./add-project-dialog.component.scss"]
 })
 export class AddProjectDialogComponent implements OnInit {
+  @Select(LobbyState.sites)
+  sites$: Observable<string[]>;
+  sites: string[] = [];
+  newSiteForm: FormGroup;
+
   constructor(
     public dialogRef: MatDialogRef<AddProjectDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: NewProjectData,
-    private store: Store
+    private store: Store,
+    private fb: FormBuilder
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.sites$.subscribe(sites => (!!sites ? (this.sites = sites) : []));
+    this.createForm();
+  }
+
+  createForm() {
+    this.newSiteForm = this.fb.group({
+      siteName: [
+        this.data.siteName,
+        [Validators.required, AddProjectDialogComponent.validateSiteName(this.sites)]
+      ],
+      siteTemplate: [this.data.siteTemplate, Validators.required],
+      username: [this.data.username, Validators.required]
+    });
+  }
+
+  static validateSiteName(sites: string[]) {
+    return (c: FormControl) =>
+      !sites.includes(c.value) ? null : { validateSiteName: { valid: false } };
+  }
 
   createSite(user) {
     this.store.dispatch(new AddSite(this.data));
