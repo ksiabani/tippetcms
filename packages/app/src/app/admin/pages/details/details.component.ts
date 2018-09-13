70fc283d6f17ef05f61cc6d9b03a80972a0f6522
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-13 15:01:21 +0300

    Implement drag n drop for page details

--- packages/app/src/app/admin/pages/details/details.component.ts
+++ packages/app/src/app/admin/pages/details/details.component.ts
@@ -1,31 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl
+} from "@angular/forms";
+import { Subscription } from "rxjs";
+import { DragulaService } from "ng2-dragula";
 
 export interface Option {
   name: string;
   value: string;
 }
 
+export interface Section {
+  name: string;
+  icon: string;
+  description: string;
+  preview: string;
+}
+
 @Component({
-  selector: 'app-details',
-  templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  selector: "app-details",
+  templateUrl: "./details.component.html",
+  styleUrls: ["./details.component.scss"]
 })
 export class DetailsComponent implements OnInit {
   pageMetaForm: FormGroup;
   paths: Option[] = [
-    {name: "/", value: "/"},
-    {name: "/blog", value: "/blog"},
-    {name: "/products", value: "/products"}
+    { name: "/", value: "/" },
+    { name: "/blog", value: "/blog" },
+    { name: "/products", value: "/products" }
   ];
+  sections: Section[] = [
+    {
+      name: "Carousel",
+      icon: "view_carousel",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+      preview: "https://picsum.photos/150/50/?image=400"
+    },
+    {
+      name: "Grid",
+      icon: "view_comfy",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+      preview: "https://picsum.photos/150/50/?image=200"
+    },
+    {
+      name: "List",
+      icon: "list",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+      preview: "https://picsum.photos/150/50/?image=100"
+    },
+    {
+      name: "Media card",
+      icon: "art_track",
+      description: "Lorem ipsum dolor sit amet consectetur adipiscing elit",
+      preview: "https://picsum.photos/150/50/?image=301"
+    }
+  ];
+  sections$ = new Subscription();
 
   constructor(
-    private fb: FormBuilder
-  ) {
-  }
+    private fb: FormBuilder,
+    private dragulaService: DragulaService
+  ) {}
 
   ngOnInit() {
     this.createForm();
+    this.sections$.add(
+      this.dragulaService.dropModel("sections").subscribe(({ targetModel }) => {
+        console.log(targetModel);
+      })
+    );
+    this.dragulaService.createGroup("sections", {
+      moves: (el, container, handle) => {
+        return handle.classList.contains("ng-handle");
+      }
+    });
   }
 
   createForm() {
