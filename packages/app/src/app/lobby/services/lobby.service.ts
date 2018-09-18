332783cf59a4b1ad22cf7d917ac48590cd99fdc8
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-18 14:28:40 +0300

    shared module

--- packages/app/src/app/lobby/services/lobby.service.ts
+++ packages/app/src/app/lobby/services/lobby.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
 import { Observable } from "rxjs";
-import { GetSitesResponse } from "../../shared/model/get-sites.interface";
+import { GetSitesResponse } from "shared";
 import { NewProjectData } from "../components/add-project-dialog/add-project-dialog.component";
 
 @Injectable({
