f910a368c8bba51588374dd5f741fd5df177f12c
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-09-21 09:57:32 +0300

    media crud

--- packages/api/src/api/admin/admin.controller.ts
+++ packages/api/src/api/admin/admin.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  UseInterceptors,
+  FileInterceptor,
+  UploadedFile,
+  Post,
+  Delete,
+} from '@nestjs/common';
 import { PagesService, TippetFile } from './pages.service';
+import { MediaService } from './media.service';
 
 @Controller('admin')
 export class AdminController {
-  constructor(private pagesService: PagesService) {}
+  constructor(private pagesService: PagesService, private mediaService: MediaService) {}
 
   // Get pages
   @Get('pages/:username/:site/:path')
@@ -23,5 +33,32 @@ export class AdminController {
     @Param('id') id: string,
   ): any {
     return this.pagesService.getSinglePage(username, site, id);
+  }
+
+  // Get a sites media
+  @Get('media/:username/:site')
+  getMedia(@Param('username') username: string, @Param('site') site: string): any {
+    return this.mediaService.listMedia(username, site);
+  }
+
+  // Upload media
+  @Post('media/:username/:site')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadMedia(
+    @Param('username') username: string,
+    @Param('site') site: string,
+    @UploadedFile() file,
+  ) {
+    return this.mediaService.uploadMedia(username, site, file);
+  }
+
+  // Remove media
+  @Delete('media/:username/:site/:mediaName')
+  removeMedia(
+    @Param('username') username: string,
+    @Param('site') site: string,
+    @Param('mediaName') mediaName: string,
+  ) {
+    return this.mediaService.removeMedia(username, site, mediaName);
   }
 }
