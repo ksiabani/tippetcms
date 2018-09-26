32031bc766c0ab1792832854ecf8880305b1c2c4
momegas <megaklis.vasilakis@gmail.com>
2018-09-26 15:47:13 +0300

    move controller endpoint logic to services

--- packages/api/src/api/api.module.ts
+++ packages/api/src/api/api.module.ts
@@ -3,9 +3,10 @@ import { LobbyController } from './lobby/lobby.controller';
 import { AdminController } from './admin/admin.controller';
 import { PagesService } from './admin/pages.service';
 import { MediaService } from './admin/media.service';
+import { SitesService } from './admin/sites.service';
 
 @Module({
   controllers: [LobbyController, AdminController],
-  providers: [PagesService, MediaService],
+  providers: [PagesService, MediaService, SitesService],
 })
 export class ApiModule {}
