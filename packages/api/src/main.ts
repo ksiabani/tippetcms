aa85e47ed31edd428f489a93a5924101a1ab4f49
Kostas Siabanis <kostas.siabanis@gmail.com>
2019-01-16 22:15:18 +0200

    API: Set global prefix

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -7,6 +7,7 @@ const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.use(express.static(path.join(__dirname, 'public')));
   app.enableCors();
   await app.listen(PORT);
