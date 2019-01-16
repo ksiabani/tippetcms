c9ef1d913269b666253e29f438cd087d9c436651
Kostas Siabanis <kostas.siabanis@gmail.com>
2019-01-16 23:26:20 +0200

    All: Add api prefix on local env too, add request timeout to 5mins

--- packages/app/src/environments/environment.ts
+++ packages/app/src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const api = "http://localhost:3000";
+const api = "http://localhost:3000/api";
 
 export const environment = {
   production: false,
