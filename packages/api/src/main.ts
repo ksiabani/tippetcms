9f0e641a06aa979a0fd8266b3dd8300530a7c5b5
Kostas Siabanis <kostas.siabanis@gmail.com>
2019-01-17 21:51:09 +0200

    Api: Housekeeping

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 import * as express from 'express';
 import * as path from 'path';
 
+// Change it to 80 if you deploy this to DO
 const PORT = 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.setGlobalPrefix('api');
   app.use(express.static(path.join(__dirname, 'public')));
   app.enableCors();
   await app.listen(PORT);
