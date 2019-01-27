dfcb673a23177ba3befbee1a0a7cf32d108b03f4
Kostas Siabanis <kostas.siabanis@gmail.com>
2019-01-27 13:31:32 +0200

    Change the order of methods

--- packages/api/src/api/admin/pages.service.ts
+++ packages/api/src/api/admin/pages.service.ts
@@ -7,6 +7,7 @@ import { Page, PageTemplate, Section, xFile } from 'shared';
 
 @Injectable()
 export class PagesService {
+  // Get all pages for a site
   getPages(username: string, site: string, path: string): xFile[] {
     const sitePath = join(__dirname, '../..', 'sites', username, site);
     const pagesJsonPath = join(sitePath, 'src', 'data', 'pages.json');
@@ -21,6 +22,7 @@ export class PagesService {
     }
   }
 
+  // Get a page
   getSinglePage(username: string, site: string, id: string): any {
     const sitePath = join(__dirname, '../..', 'sites', username, site);
     const pagesJsonPath = join(sitePath, 'src', 'data', 'pages.json');
@@ -54,6 +56,75 @@ export class PagesService {
     }
   }
 
+  // Create a page
+  addPage(
+    username: string,
+    site: string,
+    title: string,
+    path: string,
+    template: string,
+    isIndex: boolean,
+  ): Page | void {
+    // Get pages.json and site.json files
+    const sitePath = join(__dirname, '../..', 'sites', username, site);
+    const pagesJsonPath = join(sitePath, 'src', 'data', 'pages.json');
+    const siteJsonPath = join(sitePath, 'src', 'data', 'site.json');
+    try {
+      // Get existing pages
+      const pages: Page[] =
+        (existsSync(pagesJsonPath) && JSON.parse(readFileSync(pagesJsonPath, 'utf8'))) || [];
+      const fullPaths: string[] = (pages && pages.map(page => page.slug)) || [];
+
+      // Get a page from template
+      const pageFromTemplate: PageTemplate = JSON.parse(
+        readFileSync(siteJsonPath, 'utf8'),
+      ).templates.find(page => page && page.name === template);
+      const { preview, components } = pageFromTemplate;
+      const sections: Section[] = components.map(com => ({ id: shortid.generate(), ...com }));
+
+      // If the page being created is an article, give the title to the article itself
+      // and also change the default values
+      // TODO: The structure of pages.json/site.json should change to something more flexible to also
+      // accommodate this. This should only be a temp solution.
+      if (sections.length === 1 && sections[0].name === 'editor') {
+        sections[0].data.heading = title;
+        sections[0].data.description = 'Some description for your post';
+        sections[0].data.html = '';
+      }
+
+      // Create a slug. For rules for slug generation, see https://trello.com/c/UuWkeTis
+      const slug: string = isIndex
+        ? (path.length > 1 && `${path}/`) || '/'
+        : `${path}/${getSlug(title)}`;
+
+      // TODO: Validation
+      if (fullPaths.includes(slug)) {
+        throw new Error('Slug already exists');
+      }
+      // TODO: Check also if an index page already exists
+
+      // Create new page object
+      const page: Page = {
+        id: shortid.generate(),
+        template,
+        title,
+        slug,
+        preview,
+        isIndex,
+        components: sections,
+      };
+
+      // Write new page to pages.json
+      writeFileSync(pagesJsonPath, JSON.stringify([...pages, page]), 'utf8');
+
+      // Return the newly created resource
+      return page;
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  }
+
   getSection(username: string, site: string, pageId: string, id: string) {
     const sitePath = join(__dirname, '../..', 'sites', username, site);
     const pagesJsonPath = join(sitePath, 'src', 'data', 'pages.json');
@@ -65,6 +136,57 @@ export class PagesService {
     } catch (e) {
       console.log(e);
       return [];
+    }
+  }
+
+  // Create a page
+  addSection(
+    username: string,
+    site: string,
+    pageId: string,
+    title: string,
+    description: string,
+    template: string,
+  ): Page | void {
+    // Get pages.json and site.json files
+    const sitePath = join(__dirname, '../..', 'sites', username, site);
+    const pagesJsonPath = join(sitePath, 'src', 'data', 'pages.json');
+    const siteJsonPath = join(sitePath, 'src', 'data', 'site.json');
+    try {
+      const pages: Page[] = JSON.parse(readFileSync(pagesJsonPath, 'utf8'));
+      const page: Page = pages.find(page => page.id === pageId);
+      // Get components from page template
+      const pageTemplate = JSON.parse(readFileSync(siteJsonPath, 'utf8')).templates.find(
+        pageTemplate => pageTemplate && pageTemplate.name === page.template,
+      );
+      const components = pageTemplate.components;
+      // Get section from template and add position
+      const section: Section = {
+        ...components.find(com => com && com.name === template),
+        id: shortid.generate(),
+        title,
+        description,
+        position: page.components.length + 1
+      };
+      // Prepare new page object
+      const newPageObj = {
+        ...page,
+        components: [
+          ...page.components,
+          {...section}
+        ],
+      };
+      // Prepare body object
+      const body = {
+        page: newPageObj,
+      };
+      // Save page
+      this.savePage(username, site, pageId, body);
+      // Return the newly created resource
+      return newPageObj;
+    } catch (e) {
+      console.log(e);
+      return;
     }
   }
 
@@ -141,123 +263,4 @@ export class PagesService {
     return [...folders, ...files];
   }
 
-  // Create a page
-  addPage(
-    username: string,
-    site: string,
-    title: string,
-    path: string,
-    template: string,
-    isIndex: boolean,
-  ): Page | void {
-    // Get pages.json and site.json files
-    const sitePath = join(__dirname, '../..', 'sites', username, site);
-    const pagesJsonPath = join(sitePath, 'src', 'data', 'pages.json');
-    const siteJsonPath = join(sitePath, 'src', 'data', 'site.json');
-    try {
-      // Get existing pages
-      const pages: Page[] =
-        (existsSync(pagesJsonPath) && JSON.parse(readFileSync(pagesJsonPath, 'utf8'))) || [];
-      const fullPaths: string[] = (pages && pages.map(page => page.slug)) || [];
-
-      // Get a page from template
-      const pageFromTemplate: PageTemplate = JSON.parse(
-        readFileSync(siteJsonPath, 'utf8'),
-      ).templates.find(page => page && page.name === template);
-      const { preview, components } = pageFromTemplate;
-      const sections: Section[] = components.map(com => ({ id: shortid.generate(), ...com }));
-
-      // If the page being created is an article, give the title to the article itself
-      // and also change the default values
-      // TODO: The structure of pages.json/site.json should change to something more flexible to also
-      // accommodate this. This should only be a temp solution.
-      if (sections.length === 1 && sections[0].name === 'editor') {
-        sections[0].data.heading = title;
-        sections[0].data.description = 'Some description for your post';
-        sections[0].data.html = '';
-      }
-
-      // Create a slug. For rules for slug generation, see https://trello.com/c/UuWkeTis
-      const slug: string = isIndex
-        ? (path.length > 1 && `${path}/`) || '/'
-        : `${path}/${getSlug(title)}`;
-
-      // TODO: Validation
-      if (fullPaths.includes(slug)) {
-        throw new Error('Slug already exists');
-      }
-      // TODO: Check also if an index page already exists
-
-      // Create new page object
-      const page: Page = {
-        id: shortid.generate(),
-        template,
-        title,
-        slug,
-        preview,
-        isIndex,
-        components: sections,
-      };
-
-      // Write new page to pages.json
-      writeFileSync(pagesJsonPath, JSON.stringify([...pages, page]), 'utf8');
-
-      // Return the newly created resource
-      return page;
-    } catch (e) {
-      console.log(e);
-      return;
-    }
-  }
-
-  // Create a page
-  addSection(
-    username: string,
-    site: string,
-    pageId: string,
-    title: string,
-    description: string,
-    template: string,
-  ): Page | void {
-    // Get pages.json and site.json files
-    const sitePath = join(__dirname, '../..', 'sites', username, site);
-    const pagesJsonPath = join(sitePath, 'src', 'data', 'pages.json');
-    const siteJsonPath = join(sitePath, 'src', 'data', 'site.json');
-    try {
-      const pages: Page[] = JSON.parse(readFileSync(pagesJsonPath, 'utf8'));
-      const page: Page = pages.find(page => page.id === pageId);
-      // Get components from page template
-      const pageTemplate = JSON.parse(readFileSync(siteJsonPath, 'utf8')).templates.find(
-        pageTemplate => pageTemplate && pageTemplate.name === page.template,
-      );
-      const components = pageTemplate.components;
-      // Get section from template and add position
-      const section: Section = {
-        ...components.find(com => com && com.name === template),
-        id: shortid.generate(),
-        title,
-        description,
-        position: page.components.length + 1
-      };
-      // Prepare new page object
-      const newPageObj = {
-        ...page,
-        components: [
-          ...page.components,
-          {...section}
-        ],
-      };
-      // Prepare body object
-      const body = {
-        page: newPageObj,
-      };
-      // Save page
-      this.savePage(username, site, pageId, body);
-      // Return the newly created resource
-      return newPageObj;
-    } catch (e) {
-      console.log(e);
-      return;
-    }
-  }
 }
